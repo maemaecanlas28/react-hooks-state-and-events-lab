d6c1b51468f0b68811f6018c0ed40dc7d365c0f4
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-24 13:19:44 -0700

    commit tree 41de07cb7247dfabbcc07163dbebb2c7f1789431

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [addToCart, setAddToCart] = useState(false)
+
+  function handleAddToCart () {
+    setAddToCart((addToCart) => !addToCart);
+  }
+
   return (
-    <li className="">
+    <li className={addToCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        onClick={handleAddToCart}
+        className={addToCart ? "in-cart" : ""}>
+          {addToCart ? "Add to Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
